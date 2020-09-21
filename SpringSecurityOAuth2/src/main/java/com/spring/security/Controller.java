f4ab7b2e391b8903c5108330f484f585fca92f42
Ansuman Mohanty <ansumanm20@gmail.com>
2020-09-22 02:36:38 +0530

    Adding controller changes and added authentication and authorization changes based on role

--- SpringSecurityOAuth2/src/main/java/com/spring/security/Controller.java
+++ SpringSecurityOAuth2/src/main/java/com/spring/security/Controller.java
@@ -1,14 +1,25 @@
 package com.spring.security;
 
-import java.security.Principal;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
 
+//	@GetMapping("/")
+//	public String welcomeMsg(Principal principal) {
+//		return "Hi " + principal.getName() + " Welcome to OAuth2.0 Testing App";
+//	}
 	@GetMapping("/")
-	public String welcomeMsg(Principal principal) {
-		return "Hi " + principal.getName() + " Welcome to OAuth2.0 Testing App";
+	public String home() {
+		return ("<h1>Welcome All.<h1>");
 	}
+	@GetMapping("/user")
+	public String user() {
+		return ("<h1>Welcome User.<h1>");
+	}
+	@GetMapping("/admin")
+	public String admin() {
+		return ("<h1>Welcome Admin.<h1>");
+	}
+	
 }
